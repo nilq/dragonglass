6fc231801e7d8a3cd6e5aecf359f47e26fbe4249
nilq <i.am@nilq.dk>
2016-07-12 13:22:04 +0200

    Add front-end magic

--- DragonGlass/timesheet/views.py
+++ DragonGlass/timesheet/views.py
@@ -4,4 +4,4 @@ from .models import Project, Person, Time
 
 def index(request, private_id):
     person = get_object_or_404(Person, private_id=private_id)
-    return render(request, 'index.html', {'Person':person, 'Time':Time.objects})
+    return render(request, 'index.html', {'Person':person, 'Time':Time.objects, 'Project':Project.objects})
