a5d64af0b70091cb4072c9e9b09eb6a8ce28dffd
milleguy <blemille@hotmail.com>
2016-07-12 13:22:22 +0200

    models

--- DragonGlass/timesheet/models.py
+++ DragonGlass/timesheet/models.py
@@ -6,21 +6,22 @@ class Project(models.Model):
     def __str__(self):
         return self.name
     name = models.CharField(blank=True, max_length=100)
+    active = models.BooleanField(default=True)
 
 class Person(models.Model):
     def __str__(self):
         return self.name
     def end_snippet(self):
-        self.current_snippet.stop_time = datetime.datetime.now()
+        self.current_snippet.stop_time = datetime.now()
         self.current_snippet = Null
     name = models.CharField(blank=True, max_length=100)
     private_id = models.UUIDField(default=uuid.uuid4)
-    current_snippet = models.ForeignKey('Time', null=True)
+    current_snippet = models.ForeignKey('Time', null=True, blank=True)
 
 class Time(models.Model):
     def __str__(self):
         return self.project.name + " " + str(self.start_time)
-    start_time = models.DateTimeField(blank=True)
-    stop_time = models.DateTimeField(blank=True, null=True)
+    start_time = models.DateTimeField(editable=True, default=datetime.now)
+    stop_time = models.DateTimeField(null=True)
     person_safe = models.ForeignKey('Person')
     project = models.ForeignKey('Project')
