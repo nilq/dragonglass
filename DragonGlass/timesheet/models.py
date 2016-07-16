58f92ff69af7fd41e3b223fc7583977a77f76832
nilq <i.am@nilq.dk>
2016-07-16 15:57:19 +0200

    Add fixes and other bugs

--- DragonGlass/timesheet/models.py
+++ DragonGlass/timesheet/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from datetime import datetime
 import uuid
 
+class Team(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(blank=True, max_length=100)
+
 class Project(models.Model):
+    team = models.ForeignKey(Team)
     def __str__(self):
         return self.name
     name = models.CharField(blank=True, max_length=100)
@@ -13,18 +19,22 @@ class Person(models.Model):
         return self.name
     def end_snippet(self):
         self.current_snippet.stop_time = datetime.now()
-        self.current_snippet = Null
+        self.current_snippet = None
     def start_snippet(self, project):
         temp = Time.objects.create(start_time=datetime.now, person_safe=self, project=project)
         self.current_snippet = temp
+
     name = models.CharField(blank=True, max_length=100)
     private_id = models.UUIDField(default=uuid.uuid4)
-    current_snippet = models.ForeignKey('Time', null=True, blank=True)
+
+    project = models.ForeignKey(Project)
 
 class Time(models.Model):
     def __str__(self):
-        return self.project.name + " " + str(self.start_time)
-    start_time = models.DateTimeField(editable=True, default=datetime.now)
-    stop_time = models.DateTimeField(null=True)
+        return self.project.name + " : " + str(self.start_time)
+
+    start_time  = models.DateTimeField(editable = True, default = datetime.now)
+    stop_time   = models.DateTimeField(null = True, default = None)
+
     person_safe = models.ForeignKey('Person')
-    project = models.ForeignKey('Project')
+    project     = models.ForeignKey('Project')
--- DragonGlass/timesheet/views.py
+++ DragonGlass/timesheet/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project, Person, Time
 
 def index(request, private_id):
-    person = get_object_or_404(Person, private_id=private_id)
-    return render(request, 'index.html', {'Person':person, 'Time':Time.objects, 'Project':Project.objects})
+    person   = get_object_or_404(Person, private_id = private_id)
+    projects = Project.objects.all()
+    if request.method == "POST":
+        dostuff = request.POST["dostuff"]
+        if dostuff == "stop":
+            person.end_snippet()
+        else:
+            project_id = request.POST["project"]
+            person.start_snippet(Project.objects.get(id = project_id))
+
+    return render(request, 'index.html', {'Person':person, 'Time':Time.objects, 'Project':Project.objects.all()})
